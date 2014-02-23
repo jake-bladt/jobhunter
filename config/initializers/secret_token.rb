dd79672de256004e0014a4398fa79ad877093edc
Jake Bladt <jake@jakebladt.com>
2014-02-23 00:46:44 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jobhunter::Application.config.secret_key_base = 'f51ccf57f9de26080bc819c38752733fb5b36a63dfbf666573a21ac68b4bf9b428c23f979c63a12052b8a5cfd666a9714f6cb6e3c639af5c0da00f118e47ebda'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jobhunter::Application.config.session_store :cookie_store, key: '_jobhunter_session'
