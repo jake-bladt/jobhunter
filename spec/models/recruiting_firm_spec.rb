0ad7ed4da399196c12fa8d03ccd8800bdd0ba223
Jake Bladt <jake@jakebladt.com>
2014-03-01 13:24:50 -0500

    added respond_to tests

--- spec/models/recruiting_firm_spec.rb
+++ spec/models/recruiting_firm_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe RecruitingFirm do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do 
+  	@firm = RecruitingFirm.new name: 'Jobspring Partners', contact: '212.697.5800'
+  end
+  subject { @firm }
+
+  it { should respond_to :name }
+  it { should respond_to :contact }
 end
